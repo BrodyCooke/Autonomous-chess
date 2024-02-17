04dc6ef7ec42dbc2eacd353d27fae01d6a996c6d
ZachBeacon <89481556+ZachBeacon@users.noreply.github.com>
2024-02-17 15:10:05 -0500

    Added response time

--- Hall_effect/Hall_Effect.py
+++ Hall_effect/Hall_Effect.py
@@ -3,7 +3,7 @@ import time
 
 adcpin = Pin(14)
 adc = ADC(adcpin)
-# need to update the pins for wroom
+
 gpiopin1 = Pin(32, Pin.OUT)
 gpiopin2 = Pin(27, Pin.OUT)
 gpio_pin3 = Pin(13, Pin.OUT)
@@ -47,8 +47,11 @@ def read_halleffects_once():
             values.append(0)
     return values
 
-if __name == '__main':
+if __name__ == "__main__":
     '''while True:
         print(read_halleffects_cont())
         time.sleep(.5)'''
+    start = time.time_ns()
     print(read_halleffects_once())
+    end = time.time_ns()
+    print("Time to receive message from sensors: ",(end - start)/1000000000.0, " seconds")
