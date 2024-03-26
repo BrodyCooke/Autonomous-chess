6089faa92b6f6f42e250344a03993b7de284443b
ZachBeacon <89481556+ZachBeacon@users.noreply.github.com>
2024-03-26 11:17:23 -0400

    Added more Path Logic
    Move piece func, emag path, move motor

--- Microcontroller Code/pathing.py
+++ Microcontroller Code/pathing.py
@@ -1,21 +1,79 @@
 from collections import deque
 
-
+# (row,col)
 def captured_piece():
-    edges = [    # Top edge
-    [(x, 0) for x in range(8)],
-    # Right edge
-    [(7, y) for y in range(8)],
-    # Bottom edge
-    [(x, 7) for x in range(8)],
-    # Left edge
-    [(0, y) for y in range(8)]]
+    edges = [[(x, 0) for x in range(8)], [(7, y) for y in range(8)], [(x, 7) for x in range(8)], [(0, y) for y in range(8)]]
+    edges.remove(start) # removed self from edge list
     end = edges
     find_path(maze,start,end)
-    move_motor(path_list)
+    
+def move_piece(move_api):
+    #already scanned
+    # receive move from api 
+    # server sends in format (start_loc,end_loc) (5,1)
+    # scan hall effect - call get_current_board()
+    # move_api is a tuple with (start_loc,end_loc) where start and end are ordered pairs (5,1)
+    hE_board = get_current_board()
+    start_loc = move_api(0)
+    end_loc = move_api(1)
+    emag_prev = get_emag_location() # pull previous emagnet location
+
+    deactivate_emag()
+    path_list = emag_path(emag_prev, start_loc) #previous emag location, move to location
+    move_motor(path_list) # move emag to underneath desired piece to move
+    update_emag_location(end_loc) # update emag location
+
+    path_list = find_path(hE_board, start_loc,end_loc) # find the shortest path to move a piece through the board
+    activate_emag()
+    move_motor(path_list) # Move the piece through the board
+    update_emag_location(end_loc)
+    deactivate_emag()
+
+def emag_path(start, end):
+    x = start(0) - end(0)
+    y = start(0) - end(0)
+    path_list = []
+    if(x > 0):
+        for i in range(abs(x)):
+            path_list.append((0,1)) #right
+    elif(x<0):
+        for i in range(abs(x)):
+            path_list.append((0,-1)) #left
+    
+    if(y > 0):
+        for i in range(abs(y)):
+            path_list.append((1,0)) #up
+    elif(y < 0):
+        for i in range(abs(y)):
+            path_list.append((-1,0)) #down
+        
+
+
 def move_motor(path_list):
-    for x in path_list:
-        movement_dir = x[0]
+    path_count = 1
+    for i in range(len(path) -1):
+        if path[i] == path[i+1]:
+            path_count += 1
+        else: 
+            if(path[i] == (1,0)):
+                #call x positive motor
+                rotate("x", path_count) #rotate x
+            elif(path[i] == (-1,0)):
+                #call x negative motor
+                rotate("x", -path_count) #rotate x
+            elif(path[i] == (0,1)):
+                #call y positive
+                rotate("y", path_count) #rotate x
+            elif(path[i] == (0,-1)):
+                #call y negative
+                rotate("y", -path_count) #rotate x
+            else:
+                print("Cry\n")
+        
+            pass
+            # Move to Motor Code
+            #call motors
+    
         
 
 def find_path(maze, start, end):
@@ -83,11 +141,3 @@ else:
 #
 #
 #
-
-path_count = 1
-for i in range(len(path) -1):
-    if path[i] == path[i+1]:
-        path_count += 1
-    else: 
-        pass
-        #call motors
